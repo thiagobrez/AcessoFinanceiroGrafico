75dd39de5ae6b4f1965a23ef7e49a7745564a9df
John Grasel <joao.gc1997@gmail.com>
2017-11-12 16:50:09 -0200

    fix

--- AcessoFinanceiroGrafico/src/br/ufsc/ine5605/acessofinanceiro/Telas/TelaCadastroFuncionario.java
+++ AcessoFinanceiroGrafico/src/br/ufsc/ine5605/acessofinanceiro/Telas/TelaCadastroFuncionario.java
@@ -7,8 +7,12 @@ package br.ufsc.ine5605.acessofinanceiro.Telas;
 
 import br.ufsc.ine5605.acessofinanceiro.Controladores.ControladorFuncionario;
 import br.ufsc.ine5605.acessofinanceiro.Modelos.Constantes;
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.text.ParseException;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -18,6 +22,50 @@ public class TelaCadastroFuncionario extends JFrame {
 
     public TelaCadastroFuncionario(ControladorFuncionario owner) {
 
+//            Container container = getContentPane();
+//            container.setLayout(new FlowLayout());
+//
+//            //Configuracao lbNome
+//            lbNome = new JLabel(Constantes.COMUM_NOME);
+//            container.add(lbNome);
+//
+//            //Configuracao tfNome
+//            tfNome = new JTextField(5);
+//            container.add(tfNome);
+//
+//            //Configuracao lbMatricula
+//            lbMatricula = new JLabel(Constantes.COMUM_MATRICULA);
+//            container.add(lbMatricula);
+//
+//            //Configuracao tfMatricula
+//            tfMatricula = new JTextField(5);
+//            container.add(tfMatricula);
+//
+//            //Configuracao lbDataNascimento
+//            lbDataNascimento = new JLabel(Constantes.COMUM_DATA_NASCIMENTO);
+//            container.add(lbDataNascimento);
+//
+//            //Configuracao tfNascimento
+//            tfDataNascimento = new JTextField(5);
+//            container.add(tfDataNascimento);
+//
+//            //Configuracao lbTelefone
+//            lbTelefone = new JLabel(Constantes.COMUM_TELEFONE);
+//            container.add(lbTelefone);
+//
+//            //Configuracao tfTelefone
+//            tfTelefone = new JTextField(5);
+//            container.add(tfTelefone);
+//
+//            //Configuracao lbSalario
+//            lbSalario = new JLabel(Constantes.COMUM_SALARIO);
+//            container.add(lbSalario);
+//
+//            //Configuracao tfSalario
+//            tfSalario = new JTextField(5);
+//            container.add(tfSalario);
+//
+//            setVisible(visivel);
     }
-}
+
 }
