931d7dc4008ec1e0fd7ee928651b5b5fdfee52d7
John Grasel <joao.gc1997@gmail.com>
2017-11-20 13:22:28 -0200

    Front arrumado: Tela acesso (trocado o layout para gridbag) e tela registros (espacamento entre intens consertado)

--- AcessoFinanceiroGrafico/src/br/ufsc/ine5605/acessofinanceiro/Telas/TelaRegistroAcessoNegado.java
+++ AcessoFinanceiroGrafico/src/br/ufsc/ine5605/acessofinanceiro/Telas/TelaRegistroAcessoNegado.java
@@ -13,10 +13,13 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collection;
 import java.util.List;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -35,186 +38,189 @@ import javax.swing.table.DefaultTableModel;
  */
 public class TelaRegistroAcessoNegado extends JFrame {
 
-	private GridBagConstraints constraints;
-	private JScrollPane spBaseTabela;
-	private JTable tbItens;
-	private JButton btVoltar;
-	private JButton btFiltrar;
-	private JComboBox comboFiltroMotivo;
-	private JTextField tfFiltroMatricula;
-	private JLabel lbFiltroMotivo;
-	private JLabel lbFiltroMatricula;
-	private List<Integer> matriculas;
-	private JPanel painelFiltroMotivo;
-	private JPanel painelFiltroMatricula;
-	private JPanel painelBotoes;
-	
+    private GridBagConstraints constraints;
+    private JScrollPane spBaseTabela;
+    private JTable tbItens;
+    private JButton btVoltar;
+    private JButton btFiltrar;
+    private JComboBox comboFiltroMotivo;
+    private JTextField tfFiltroMatricula;
+    private JLabel lbFiltroMotivo;
+    private JLabel lbFiltroMatricula;
+    private List<Integer> matriculas;
+    private JPanel painelFiltroMotivo;
+    private JPanel painelFiltroMatricula;
+    private JPanel painelBotoes;
 
     public TelaRegistroAcessoNegado() {
-		super(Constantes.REGISTRO_TITULO);
-		configuraTela();
+        super(Constantes.REGISTRO_TITULO);
+        configuraTela();
     }
 
-	/**
-	 * Configura os elementos que aparecerao na interface grafica.
-	 */
-	private void configuraTela() {
-		
-		//Configuracao container
-		Container container = getContentPane();
-		container.setLayout(new GridBagLayout());
+    /**
+     * Configura os elementos que aparecerao na interface grafica.
+     */
+    private void configuraTela() {
 
-		//Configuracao constraints
-		this.constraints = new GridBagConstraints();
-		constraints.fill = GridBagConstraints.CENTER;
+        //Configuracao container
+        Container container = getContentPane();
+        container.setLayout(new GridBagLayout());
+
+        //Configuracao constraints
+        this.constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.CENTER;
+        constraints.insets = new Insets(7, 7, 7, 7);
         constraints.gridwidth = 15;
         constraints.gridheight = 15;
         constraints.gridx = 0;
         constraints.gridy = 0;
-		
-		//Configuracao tbItens
-		this.tbItens = new JTable();
-		tbItens.setAutoCreateRowSorter(true);
-		tbItens.setPreferredScrollableViewportSize(new Dimension(500, 300));
-		tbItens.setFillsViewportHeight(true);
-		spBaseTabela = new JScrollPane(tbItens);
-		container.add(spBaseTabela, constraints);
-		
-		//Configuracao btManager
-		GerenciadorBotoes btManager = new GerenciadorBotoes();
-		
-		//Configuracao lbFiltroMotivo
-		lbFiltroMotivo = new JLabel(Constantes.REGISTRO_LABEL_FILTRO_MOTIVO);
-		
-		//Configuracao comboFiltroMotivo
-		Object[] motivos = {
-			Constantes.REGISTRO_FILTRO_TODOS,
-			Motivo.MATRICULA_INEXISTENTE,
-			Motivo.HORARIO_NAO_PERMITIDO,
-			Motivo.CARGO_SEM_ACESSO,
-			Motivo.ACESSO_BLOQUEADO
-		};
-		comboFiltroMotivo = new JComboBox(motivos);
-		
-		//Configuracao lbFiltroMatricula
-		lbFiltroMatricula = new JLabel(Constantes.REGISTRO_LABEL_FILTRO_MATRICULA);
-		
-		//Configuracao tfFiltroMatricula
-		tfFiltroMatricula = new JTextField(10);
-		
-		//Configuracao btFiltrar
-		btFiltrar = new JButton(Constantes.REGISTRO_BOTAO_FILTRAR);
-		btFiltrar.addActionListener(btManager);
-		
-		//Configuracao btVoltar
-		btVoltar = new JButton(Constantes.COMUM_BOTAO_VOLTAR);
-		btVoltar.addActionListener(btManager);
-		
-		//Configuracao painelFiltroMotivo
+
+        //Configuracao tbItens
+        this.tbItens = new JTable();
+        tbItens.setAutoCreateRowSorter(true);
+        tbItens.setPreferredScrollableViewportSize(new Dimension(500, 300));
+        tbItens.setFillsViewportHeight(true);
+        spBaseTabela = new JScrollPane(tbItens);
+        container.add(spBaseTabela, constraints);
+
+        //Configuracao btManager
+        GerenciadorBotoes btManager = new GerenciadorBotoes();
+
+        //Configuracao lbFiltroMotivo
+        lbFiltroMotivo = new JLabel(Constantes.REGISTRO_LABEL_FILTRO_MOTIVO);
+
+        //Configuracao comboFiltroMotivo
+        Object[] motivos = {
+            Constantes.REGISTRO_FILTRO_TODOS,
+            Motivo.MATRICULA_INEXISTENTE,
+            Motivo.HORARIO_NAO_PERMITIDO,
+            Motivo.CARGO_SEM_ACESSO,
+            Motivo.ACESSO_BLOQUEADO
+        };
+        comboFiltroMotivo = new JComboBox(motivos);
+
+        //Configuracao lbFiltroMatricula
+        lbFiltroMatricula = new JLabel(Constantes.REGISTRO_LABEL_FILTRO_MATRICULA);
+
+        //Configuracao tfFiltroMatricula
+        tfFiltroMatricula = new JTextField(10);
+
+        //Configuracao btFiltrar
+        btFiltrar = new JButton(Constantes.REGISTRO_BOTAO_FILTRAR);
+        btFiltrar.addActionListener(btManager);
+
+        //Configuracao btVoltar
+        btVoltar = new JButton(Constantes.COMUM_BOTAO_VOLTAR);
+        btVoltar.addActionListener(btManager);
+
+        //Configuracao painelFiltroMotivo
         this.painelFiltroMotivo = new JPanel();
         painelFiltroMotivo.setLayout(new BoxLayout(painelFiltroMotivo, BoxLayout.LINE_AXIS));
         this.painelFiltroMotivo.setVisible(true);
-		painelFiltroMotivo.add(lbFiltroMotivo);
-		painelFiltroMotivo.add(comboFiltroMotivo);
-		constraints.gridy = 20;
+        painelFiltroMotivo.add(lbFiltroMotivo);
+        painelFiltroMotivo.add(comboFiltroMotivo);
+        constraints.gridy = 20;
         container.add(painelFiltroMotivo, constraints);
 
-		//Configuracao painelFiltroMatricula
+        //Configuracao painelFiltroMatricula
         this.painelFiltroMatricula = new JPanel();
         painelFiltroMatricula.setLayout(new BoxLayout(painelFiltroMatricula, BoxLayout.LINE_AXIS));
         this.painelFiltroMatricula.setVisible(true);
-		painelFiltroMatricula.add(lbFiltroMatricula);
-		painelFiltroMatricula.add(tfFiltroMatricula);
-		constraints.gridy = 40;
+        painelFiltroMatricula.add(lbFiltroMatricula);
+        painelFiltroMatricula.add(tfFiltroMatricula);
+        constraints.gridy = 40;
         container.add(painelFiltroMatricula, constraints);
-		
-		//Configuracao painelBotoes
-		this.painelBotoes = new JPanel();
-		painelBotoes.setLayout(new BoxLayout(painelBotoes, BoxLayout.LINE_AXIS));
-		this.painelBotoes.setVisible(true);
-		painelBotoes.add(btFiltrar);
+
+        //Configuracao painelBotoes
+        this.painelBotoes = new JPanel();
+        painelBotoes.setLayout(new BoxLayout(painelBotoes, BoxLayout.LINE_AXIS));
+        painelBotoes.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+        this.painelBotoes.setVisible(true);
+        painelBotoes.add(Box.createHorizontalGlue());
         painelBotoes.add(btVoltar);
-		constraints.gridy = 60;
-		container.add(painelBotoes, constraints);
-		
-		setSize(600, 500);
-		setLocationRelativeTo(null);
-	}
-	
-	/**
-	 * Atualiza os dados da tabela de registros.
-	 * 
-	 * @param filtroMotivo motivo recebido para filtrar os itens mostrados
-	 * @param filtroMatricula matricula recebida para filtrar os itens mostrados
-	 */
-	public void updateData(String filtroMotivo, String filtroMatricula) {
-		
-		//Configuracao modelTbItens
-		DefaultTableModel modelTbItens = new DefaultTableModel() {
-			@Override
-			public boolean isCellEditable(int row, int column) {
-				return false;
-			}
-		};
-		modelTbItens.addColumn(Constantes.COMUM_DATA);
-		modelTbItens.addColumn(Constantes.COMUM_MATRICULA);
-		modelTbItens.addColumn(Constantes.REGISTRO_MOTIVO);
-		
-		Collection<RegistroAcessoNegado> listaRegistros = ControladorRegistroAcessoNegado
-				.getInstance()
-				.filtraRegistros(filtroMotivo, filtroMatricula);
-		
-		for(RegistroAcessoNegado registro : listaRegistros) {
-			modelTbItens.addRow(new Object[]{
-				registro.getData(),
-				registro.getMatricula(),
-				registro.getMotivo()
-			});
-		}
-		tbItens.setModel(modelTbItens);
-		
-		this.repaint();
-	}
-	
-    /**
-	 * Chama a funcao para atualizar os dados da tabela e exibe o relatorio.
-	 */
-    public void exibeMenuRelatorio() {
-		updateData(Constantes.REGISTRO_FILTRO_TODOS, Constantes.REGISTRO_FILTRO_VAZIO);
-		setVisible(true);
+        painelBotoes.add(Box.createRigidArea(new Dimension(10, 0)));
+        painelBotoes.add(btFiltrar);
+        constraints.gridy = 60;
+        container.add(painelBotoes, constraints);
+
+        setSize(600, 500);
+        setLocationRelativeTo(null);
     }
 
-	/**
-	 * Exibe modal indicando que a matricula inserida para filtro da tabela eh
-	 * invalida.
-	 */
-	public void exibeFiltroMatriculaInvalido() {
-		JOptionPane.showMessageDialog(
-			null,
-			Constantes.REGISTRO_FILTRO_MATRICULA_INVALIDA,
-			Constantes.REGISTRO_FILTRO_INVALIDO,
-			JOptionPane.PLAIN_MESSAGE
-		);
-	}
+    /**
+     * Atualiza os dados da tabela de registros.
+     *
+     * @param filtroMotivo motivo recebido para filtrar os itens mostrados
+     * @param filtroMatricula matricula recebida para filtrar os itens mostrados
+     */
+    public void updateData(String filtroMotivo, String filtroMatricula) {
 
-	private class GerenciadorBotoes implements ActionListener {
+        //Configuracao modelTbItens
+        DefaultTableModel modelTbItens = new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        modelTbItens.addColumn(Constantes.COMUM_DATA);
+        modelTbItens.addColumn(Constantes.COMUM_MATRICULA);
+        modelTbItens.addColumn(Constantes.REGISTRO_MOTIVO);
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(e.getSource().equals(btVoltar)) {
-				setVisible(false);
-			} else if(e.getSource().equals(btFiltrar)) {
-				if(ControladorRegistroAcessoNegado.getInstance().verificaFiltroMatricula(tfFiltroMatricula.getText())) {
-					updateData(
-						comboFiltroMotivo.getSelectedItem().toString(),
-						tfFiltroMatricula.getText()
-					);
-				} else {
-					exibeFiltroMatriculaInvalido();
-				}
-			}
-		}
-		
-	}
-	
+        Collection<RegistroAcessoNegado> listaRegistros = ControladorRegistroAcessoNegado
+                .getInstance()
+                .filtraRegistros(filtroMotivo, filtroMatricula);
+
+        for (RegistroAcessoNegado registro : listaRegistros) {
+            modelTbItens.addRow(new Object[]{
+                registro.getData(),
+                registro.getMatricula(),
+                registro.getMotivo()
+            });
+        }
+        tbItens.setModel(modelTbItens);
+
+        this.repaint();
+    }
+
+    /**
+     * Chama a funcao para atualizar os dados da tabela e exibe o relatorio.
+     */
+    public void exibeMenuRelatorio() {
+        updateData(Constantes.REGISTRO_FILTRO_TODOS, Constantes.REGISTRO_FILTRO_VAZIO);
+        setVisible(true);
+    }
+
+    /**
+     * Exibe modal indicando que a matricula inserida para filtro da tabela eh
+     * invalida.
+     */
+    public void exibeFiltroMatriculaInvalido() {
+        JOptionPane.showMessageDialog(
+                null,
+                Constantes.REGISTRO_FILTRO_MATRICULA_INVALIDA,
+                Constantes.REGISTRO_FILTRO_INVALIDO,
+                JOptionPane.PLAIN_MESSAGE
+        );
+    }
+
+    private class GerenciadorBotoes implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource().equals(btVoltar)) {
+                setVisible(false);
+            } else if (e.getSource().equals(btFiltrar)) {
+                if (ControladorRegistroAcessoNegado.getInstance().verificaFiltroMatricula(tfFiltroMatricula.getText())) {
+                    updateData(
+                            comboFiltroMotivo.getSelectedItem().toString(),
+                            tfFiltroMatricula.getText()
+                    );
+                } else {
+                    exibeFiltroMatriculaInvalido();
+                }
+            }
+        }
+
+    }
+
 }
